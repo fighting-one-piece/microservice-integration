e10f99d72898f5b45cd57def2779a79f3df3fbba
wulin <125906088@qq.com>
2017-06-09 20:30:57 +0800

    base project code modify commit

---
+++ ms-base/src/main/java/org/cisiondata/modules/abstr/service/converter/IConverter.java
@@ -0,0 +1,45 @@
+package org.cisiondata.modules.abstr.service.converter;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+/** 对象转换*/
+public interface IConverter<Entity extends Serializable, EntityDTO extends Serializable> {
+
+	/**
+	 * 转换对象(双向)
+	 * @param object
+	 * @return
+	 */
+	public Object convertObject(Object object);
+
+	/**
+	 * 转换对象
+	 * @param entity
+	 * @param entityDTO
+	 */
+	public void convertEntity2DTO(Entity entity, EntityDTO entityDTO) ;
+
+	/**
+	 * 转换对象
+	 * @param entityDTO
+	 * @param entity
+	 */
+	public void convertDTO2Entity(EntityDTO entityDTO, Entity entity) ;
+
+	/**
+	 * 转换对象
+	 * @param entityCollection
+	 * @param entityDTOCollection
+	 */
+	public void convertEntityCollection2DTOCollection(
+			Collection<Entity> entityCollection, Collection<EntityDTO> entityDTOCollection) ;
+
+	/**
+	 * 转换对象
+	 * @param entityDTOCollection
+	 * @param entityCollection
+	 */
+	public void convertDTOCollection2EntityCollection(
+			Collection<EntityDTO> entityDTOCollection, Collection<Entity> entityCollection) ;
+}
---
+++ ms-base/src/main/java/org/cisiondata/utils/json/MapDeserializer.java
@@ -0,0 +1,55 @@
+package org.cisiondata.utils.json;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;  
+
+public class MapDeserializer implements JsonDeserializer<Map<String, Object>> {
+
+	/**
+	 * 默认会将Object接收的数字转换为double 
+	 * int类型12转换后会变成12.0，自定义转换的目的就是将他转换为12
+	 */
+	public Map<String, Object> deserialize(JsonElement json, Type type, JsonDeserializationContext context)
+			throws JsonParseException {
+		Map<String, Object> map = new HashMap<String, Object>();
+		Set<Entry<String, JsonElement>> entries = json.getAsJsonObject().entrySet();
+		for (Entry<String, JsonElement> entry : entries) {
+			JsonElement element = entry.getValue();
+			if (null == element || element instanceof JsonNull) continue;
+			Class<?> elementClass = element.getClass();
+			if (JsonArray.class.isAssignableFrom(elementClass)) {
+				map.put(entry.getKey(), GsonUtils.builder().toJson(element));
+			} else if (JsonObject.class.isAssignableFrom(elementClass)) {
+				map.put(entry.getKey(), GsonUtils.builder().toJson(element));
+			} else {
+				map.put(entry.getKey(), isNumberic(element.getAsString()) ? element.getAsLong() : element.getAsString());
+			}
+		}
+		return map;
+	}
+
+	/**
+	 * 判断是不是数字
+	 * @param input
+	 * @return 是数字类型返回true
+	 */
+	public boolean isNumberic(String input) {
+		Pattern pattern = Pattern.compile("[0-9]+");
+		Matcher matcher = pattern.matcher(input);
+		return !matcher.matches() ? false : true;
+	}
+
+}
