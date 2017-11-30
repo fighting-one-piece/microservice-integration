8c8a0a68fe026e82f7f0c0dd430e612ca9afdacc
wulin <125906088@qq.com>
2017-11-30 18:49:09 +0800

    devplat basic code modify commit

--- devplat-base/src/main/java/org/cisiondata/modules/bootstrap/BootstrapApplication.java
+++ devplat-base/src/main/java/org/cisiondata/modules/bootstrap/BootstrapApplication.java
@@ -3,25 +3,26 @@ package org.cisiondata.modules.bootstrap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
 @EnableSwagger2
-@SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan(basePackages = { "org.cisiondata" })
-public class BootstrapDataPlat extends BootstrapApplication {
+@EnableHystrix
+@EnableEurekaClient
+@EnableFeignClients(basePackages = {"org.cisiondata.modules"})
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+public class BootstrapApplication extends BaseBootstrapApplication {
 
-	public static Logger LOG = LoggerFactory.getLogger(BootstrapDataPlat.class);
-
+	private static Logger LOG = LoggerFactory.getLogger(BaseBootstrapApplication.class);
+	
 	public static void main(String[] args) {
-		SpringApplication.run(BootstrapDataPlat.class, args);
-		LOG.info("DataPlat Server Bootstrap");
+		SpringApplication.run(BootstrapApplication.class, args);
+		LOG.info("DevPlat Server Bootstrap");
 	}
-
 }
--- devplat-base/src/test/java/org/cisiondata/modules/boot/ServiceTest.java
+++ devplat-base/src/test/java/org/cisiondata/modules/boot/ServiceTest.java
@@ -1,13 +1,13 @@
 package org.cisiondata.modules.boot;
 
-import org.cisiondata.modules.bootstrap.BootstrapApplication;
+import org.cisiondata.modules.bootstrap.BaseBootstrapApplication;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = BootstrapApplication.class)
+@SpringBootTest(classes = BaseBootstrapApplication.class)
 @WebAppConfiguration 
 public class ServiceTest {
 	
