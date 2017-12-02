2e9775fd6c7cf36198176e24b119e909f8b2daed
wulin <wulin.198804@gmail.com>
2017-12-03 00:38:15 +0800

    elastic5 related code modify commit

---
+++ ms-elastic5/src/main/java/org/cisiondata/modules/elastic/entity/TempDoc.java
@@ -0,0 +1,43 @@
+package org.cisiondata.modules.elastic.entity;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id; 
+import org.springframework.data.elasticsearch.annotations.Document;  
+
+@Document(indexName="es_temp_inx", type="temp_doc", indexStoreType="fs", shards=5, replicas=1, refreshInterval="-1")
+public class TempDoc implements Serializable {
+
+	@Id  
+    private Long id = null;
+	
+	private String title = null;
+	
+	private String content = null;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
