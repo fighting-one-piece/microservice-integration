2f063bfa7e55f39812e17b90049dadd32cafaf85
wulin <125906088@qq.com>
2017-12-02 18:34:01 +0800

    subversion related code modify commit

--- ms-config-server/src/main/java/org/cisiondata/modules/bootstrap/BootstrapApplication.java
+++ ms-config-server/src/main/java/org/cisiondata/modules/bootstrap/BootstrapApplication.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableConfigServer
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableAutoConfiguration  
 @ComponentScan(basePackages={"org.cisiondata"})
 public class BootstrapApplication {
