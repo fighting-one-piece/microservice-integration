79c7b454650321536537c6f603d68743c8c3eda2
wulin <125906088@qq.com>
2017-12-08 19:29:46 +0800

    devplat related code modify commit

--- devplat-base/src/test/java/org/cisiondata/modules/boot/DAOTest.java
+++ devplat-base/src/test/java/org/cisiondata/modules/boot/DAOTest.java
@@ -3,12 +3,22 @@ package org.cisiondata.modules.boot;
 import org.cisiondata.modules.bootstrap.BaseBootstrapApplication;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BaseBootstrapApplication.class)
 @WebAppConfiguration 
+@EnableHystrix
+@EnableEurekaClient
+@EnableFeignClients(basePackages = {"org.cisiondata.modules"})
+@EnableCircuitBreaker
+@EnableDiscoveryClient
 public class DAOTest {
-
+	
 }
