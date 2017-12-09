df0cc68970d786bbb0f0a25c0d0f1f83cd5545a7
wulin <125906088@qq.com>
2017-12-09 17:51:10 +0800

    modify log config and add error page

--- ms-elastic2/src/main/java/org/cisiondata/modules/bootstrap/BootstrapApplication.java
+++ ms-elastic2/src/main/java/org/cisiondata/modules/bootstrap/BootstrapApplication.java
@@ -5,10 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -30,6 +34,19 @@ public class BootstrapApplication {
 		objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
 		objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 		return objectMapper;
+	}
+	
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {    
+	    return new EmbeddedServletContainerCustomizer(){        
+	        @Override        
+	         public void customize(ConfigurableEmbeddedServletContainer container) {            
+	            container.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/error/400.html"));            
+	            container.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, "/error/401.html"));            
+	            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error/404.html"));        
+	            container.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500.html"));            
+	        }    
+	    };
 	}
 	
 	public static void main(String[] args) {
