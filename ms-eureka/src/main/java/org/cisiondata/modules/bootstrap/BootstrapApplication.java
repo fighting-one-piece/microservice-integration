eec130da9d3b7ac8e5746456c011be75b4fc0290
wulin <125906088@qq.com>
2017-12-22 18:42:51 +0800

    add error mapping handle

--- ms-eureka/src/main/java/org/cisiondata/modules/bootstrap/BootstrapApplication.java
+++ ms-eureka/src/main/java/org/cisiondata/modules/bootstrap/BootstrapApplication.java
@@ -5,8 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication  
 @EnableEurekaServer
@@ -15,6 +20,19 @@ import org.springframework.context.annotation.ComponentScan;
 public class BootstrapApplication {
 
 	private static Logger LOG = LoggerFactory.getLogger(BootstrapApplication.class);
+	
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {    
+	    return new EmbeddedServletContainerCustomizer(){        
+	        @Override        
+	         public void customize(ConfigurableEmbeddedServletContainer container) {            
+	            container.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/error/400.html"));            
+	            container.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, "/error/401.html"));            
+	            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error/404.html"));        
+	            container.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500.html"));            
+	        }    
+	    };
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BootstrapApplication.class, args);
