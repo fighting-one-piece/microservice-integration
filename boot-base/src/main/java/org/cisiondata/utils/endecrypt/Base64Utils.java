690dcef6cef331ae7e432216a3b0a6ad8c0575fe
wulin <125906088@qq.com>
2018-02-28 19:25:59 +0800

    modify base64 utils

--- boot-base/src/main/java/org/cisiondata/utils/endecrypt/Base64Utils.java
+++ boot-base/src/main/java/org/cisiondata/utils/endecrypt/Base64Utils.java
@@ -1,6 +1,8 @@
 package org.cisiondata.utils.endecrypt;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,14 +12,35 @@ public class Base64Utils {
 	private static Logger LOG = LoggerFactory.getLogger(Base64Utils.class);
 
 	public static String encode(byte[] input) {
+		return Base64.getEncoder().encodeToString(input);
+	}
+	
+	public static String encode(String input) {
+		return Base64.getEncoder().encodeToString(input.getBytes());
+	}
+	
+	public static String encodeWithSunMisc(byte[] input) {
 		return new sun.misc.BASE64Encoder().encode(input);
 	}
 
-	public static String encode(String input) {
+	public static String encodeWithSunMisc(String input) {
 		return new sun.misc.BASE64Encoder().encode(input.getBytes());
 	}
 
 	public static byte[] decode(String input) {
+		return Base64.getDecoder().decode(input);
+	}
+	
+	public static String decode(String input, String charset) {
+		try {
+			return new String(Base64.getDecoder().decode(input), charset);
+		} catch (UnsupportedEncodingException e) {
+			LOG.error(e.getMessage(), e);
+		}
+		return null;
+	}
+	
+	public static byte[] decodeWithSunMisc(String input) {
 		try {
 			sun.misc.BASE64Decoder decoder = new sun.misc.BASE64Decoder();
 			return decoder.decodeBuffer(input);
@@ -27,7 +50,7 @@ public class Base64Utils {
 		return null;
 	}
 	
-	public static String decode(String input, String charset) {
+	public static String decodeWithSunMisc(String input, String charset) {
 		try {
 			sun.misc.BASE64Decoder decoder = new sun.misc.BASE64Decoder();
 			return new String(decoder.decodeBuffer(input), charset);
