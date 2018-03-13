a3da4cf22ba5bae6ec32e2b92e850d1184c11dcc
wulin <125906088@qq.com>
2018-03-13 11:32:58 +0800

    add job and task

---
+++ boot-base/src/main/java/org/cisiondata/modules/scheduler/Task.java
@@ -0,0 +1,18 @@
+package org.cisiondata.modules.scheduler;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class Task implements Runnable {
+	
+	protected Logger LOG = LoggerFactory.getLogger(getClass());
+	
+	protected Map<String, Object> params = null;
+	
+	public void setParams(Map<String, Object> params) {
+		this.params = params;
+	}
+	
+}
