02e8ba351a916da8424935b6ba5b664ff5883573
wulin <125906088@qq.com>
2019-04-25 15:51:51 +0800

    modify scheduler related code

--- boot-base-1/src/main/java/org/platform/modules/scheduler/Task.java
+++ boot-base-1/src/main/java/org/platform/modules/scheduler/Task.java
@@ -2,6 +2,7 @@ package org.platform.modules.scheduler;
 
 import java.util.Map;
 
+import org.platform.utils.redis.RedisClusterUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +14,14 @@ public abstract class Task implements Runnable {
 	
 	public void setParams(Map<String, Object> params) {
 		this.params = params;
+	}
+	
+	public void put(String qname, Object object) {
+		RedisClusterUtils.getInstance().listPush(qname, object);
+	}
+	
+	public Object take(String qname) {
+		return RedisClusterUtils.getInstance().listPop(qname);
 	}
 	
 }
