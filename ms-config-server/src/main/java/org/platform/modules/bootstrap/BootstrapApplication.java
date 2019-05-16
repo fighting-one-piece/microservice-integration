4c4f14b8c0dc083fe682f3b1551761a10eb603f6
wulin <125906088@qq.com>
2019-05-16 18:44:08 +0800

    add spring cloud bus config

--- ms-config-server/src/main/java/org/platform/modules/bootstrap/BootstrapApplication.java
+++ ms-config-server/src/main/java/org/platform/modules/bootstrap/BootstrapApplication.java
@@ -6,11 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableConfigServer
-@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableAutoConfiguration  
 @ComponentScan(basePackages={"org.platform"})
