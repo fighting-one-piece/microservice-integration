f5e1423d41ca8f9c4937357602728c8afee7af26
wulin <125906088@qq.com>
2019-06-21 19:23:41 +0800

    add search guard plugin support

--- ms-elasticsearch-2/src/main/java/org/platform/modules/elastic/utils/ElasticClient.java
+++ ms-elasticsearch-2/src/main/java/org/platform/modules/elastic/utils/ElasticClient.java
@@ -1,5 +1,6 @@
 package org.platform.modules.elastic.utils;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
@@ -15,6 +16,9 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.shield.ShieldPlugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.floragunn.searchguard.ssl.SearchGuardSSLPlugin;
+import com.floragunn.searchguard.ssl.util.SSLConfigConstants;
 
 public class ElasticClient {
 	
@@ -66,9 +70,26 @@ public class ElasticClient {
 				String[] ipAndPort = clusterNode.split(":");
 				esServerAddress.add(new EsServerAddress(ipAndPort[0], Integer.parseInt(ipAndPort[1])));
 			}
+			String storePath = ElasticClient.class.getClassLoader().getResource(
+					properties.getProperty("es.searchGuard.store-path")).getPath();
+			storePath = storePath.startsWith("/") ? storePath.substring(1) : storePath;
+			String keystoreJks = properties.getProperty("es.searchGuard.keystore-jks");
+			String keystorePassword = properties.getProperty("es.searchGuard.keystore-password");
+			String truststoreJks = properties.getProperty("es.searchGuard.truststore-jks");
+			String truststorePassword = properties.getProperty("es.searchGuard.truststore-password");
+			boolean enforceHostnameVerification = Boolean.parseBoolean(
+				properties.getProperty("es.searchGuard.enforce-hostname-verification", "false"));
 			Settings settings = Settings.builder().put("cluster.name", clusterName)
-					.put("shield.user", "guest:@#guest123").put("client.transport.sniff", true).build();
-			client = TransportClient.builder().addPlugin(ShieldPlugin.class).settings(settings).build();
+					.put("shield.user", "guest:@#guest123").put("client.transport.sniff", true)
+					.put(SSLConfigConstants.SEARCHGUARD_SSL_HTTP_ENABLED, true)
+		            .put(SSLConfigConstants.SEARCHGUARD_SSL_TRANSPORT_KEYSTORE_FILEPATH, storePath + File.separator + keystoreJks)
+		            .put(SSLConfigConstants.SEARCHGUARD_SSL_TRANSPORT_KEYSTORE_PASSWORD, keystorePassword)
+		            .put(SSLConfigConstants.SEARCHGUARD_SSL_TRANSPORT_TRUSTSTORE_FILEPATH, storePath + File.separator + truststoreJks)
+		            .put(SSLConfigConstants.SEARCHGUARD_SSL_TRANSPORT_TRUSTSTORE_PASSWORD, truststorePassword)
+		            .put(SSLConfigConstants.SEARCHGUARD_SSL_TRANSPORT_ENFORCE_HOSTNAME_VERIFICATION, enforceHostnameVerification)
+					.build();
+			client = TransportClient.builder().addPlugin(ShieldPlugin.class)
+				.addPlugin(SearchGuardSSLPlugin.class).settings(settings).build();
 			for (EsServerAddress address : esServerAddress) {
 				client.addTransportAddress(new InetSocketTransportAddress(
 					new InetSocketAddress(address.getHost(), address.getPort())));
